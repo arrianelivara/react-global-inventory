7799bd019710e6d5c6e1ff1385ec86aa15f695e0
arriane-livara <arriane.livara@pnp-hcl.com>
2022-12-20 11:41:54 +0800

    fix warehouse

--- src/modules/master-data/common/warehouse.module.jsx
+++ src/modules/master-data/common/warehouse.module.jsx
@@ -1,24 +1,28 @@
+import { getAllWarehouse } from 'apis/warehouse.api';
 import { Field, Select } from 'components';
+import { useApi, useMount } from 'hooks/index';
+import { warehouseOptions } from 'mappers/warehouse.mapper';
 import React from 'react';
 import lang from "translations";
 
 const WarehouseSelection = ({ field, modifyField }) => {
 
-    const sampleWarehouses = [
-        {
-            value: 0,
-            text: "All",
-        },
-        {
-            value: 1,
-            text: "Warehouse 1",
-        }
-    ];
+    const { request, loading ,
+        mappedData } = useApi({
+        api: getAllWarehouse,
+        isArray: true,
+        mapper: warehouseOptions
+    });
+
+
+    useMount(() => {
+        request();
+    });
 
     return (
         <div className="w-1/4">
             <Field label={lang.warehouseLabel} {...field}>
-                <Select {...field} onChange={modifyField} options={sampleWarehouses} text={lang.filterByWarehouse} />
+                <Select value="All" loading={loading} {...field} onChange={modifyField} text={lang.filterByWarehouse} options={[{value: "All", text: "All"}, ...mappedData]} />
             </Field>
         </div>
     );
